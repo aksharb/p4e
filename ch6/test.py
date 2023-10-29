5dd36737db478ae63ea14f0a069ff95586bfd472
Akshar Bisht <bisht.akshar@gmail.com>
2023-10-29 12:41:36 +0530

    used for loop

--- ch6/test.py
+++ ch6/test.py
@@ -1,8 +1,3 @@
 fruit = "banana"
-x = len(fruit)
-print(x)
-index = 0
-while index < len(fruit):
-    letter = fruit[index]
-    print(index, letter)
-    index = index + 1
+for letter in fruit:
+    print(letter)
